80d131badc5f8fc383c668ee56d02629011191ca
Dani Saavedra <daniel.saavedra@mercadolibre.com.co>
2022-09-26 20:01:58 -0500

    Test fail commit

--- src/main/java/co/edu/unisabana/usuario/clan/EmployeeRepository.java
+++ src/main/java/co/edu/unisabana/usuario/clan/EmployeeRepository.java
@@ -2,7 +2,7 @@ package co.edu.unisabana.usuario.clan;
 
 public class EmployeeRepository {
 
-    public String searchEmployee(String name) {
+    public Strxing searchEmployee(String name) {
         //Base datos (SQL o noSQL, REST, SOAP)
         throw new RuntimeException("Sin implementar aún");
     }
