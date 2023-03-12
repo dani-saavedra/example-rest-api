884730711e0f6c0c8446ea7340050ca3faba5bcd
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2023-03-11 19:14:07 -0500

    Remove unncessary parametern in RegiisterBookLibrary

--- src/main/java/co/edu/unisabana/usuario/service/library/RegisterBookLibrary.java
+++ src/main/java/co/edu/unisabana/usuario/service/library/RegisterBookLibrary.java
@@ -9,31 +9,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class RegisterBookLibrary {
 
-    private final SearchBookPort searchBookPort;
-    private final AddBookPort addBookPort;
-    private final RegisterBookPort registerBookPort;
+  private final SearchBookPort searchBookPort;
+  private final AddBookPort addBookPort;
+  private final RegisterBookPort registerBookPort;
 
-    private Suma suma;
+  public RegisterBookLibrary(SearchBookPort searchBookPort, AddBookPort addBookPort,
+      RegisterBookPort registerBookPort) {
+    this.searchBookPort = searchBookPort;
+    this.addBookPort = addBookPort;
+    this.registerBookPort = registerBookPort;
+  }
 
-    public RegisterBookLibrary(SearchBookPort searchBookPort, AddBookPort addBookPort, RegisterBookPort registerBookPort, Suma objectoInyectado) {
-        this.searchBookPort = searchBookPort;
-        this.addBookPort = addBookPort;
-        this.registerBookPort = registerBookPort;
-        this.suma = objectoInyectado;
+
+  public int registerBook(Book book) {
+    boolean exists = searchBookPort.validateExistsBook(book.getName());
+    if (exists) {
+      addBookPort.addBook(book.getName());
+      return 1;
+    } else {
+      registerBookPort.registerBook(book);
+      return 2;
     }
-
-
-    public int registerBook(Book book) {
-        suma.sumar();
-        boolean exists = searchBookPort.validateExistsBook(book.getName());
-        if (exists) {
-            addBookPort.addBook(book.getName());
-            return 1;
-        } else {
-            registerBookPort.registerBook(book);
-            return 2;
-        }
-    }
+  }
 
 
 }
