40e060058ee0637562ea0f075bcaaed271853e4a
Dani Saavedra <daniel.saavedra@mercadolibre.com.co>
2023-03-23 12:48:18 -0500

    Revert "Implementation mysql"
    This reverts commit dab87d8684dfa2393695739435317bc451352530.

--- src/main/java/co/edu/unisabana/usuario/repository/dao/entity/BookEntity.java
+++ src/main/java/co/edu/unisabana/usuario/repository/dao/entity/BookEntity.java
@@ -1,8 +1,6 @@
 package co.edu.unisabana.usuario.repository.dao.entity;
 
 import co.edu.unisabana.usuario.service.library.model.Book;
-import javax.persistence.Entity;
-import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +8,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
 public class BookEntity {
 
     private String name;
@@ -18,8 +15,7 @@ public class BookEntity {
     private String author;
     private boolean rRated;
     private String category;
-    @Id
-    private Integer id;
+    private int id;
     private int quantity;
 
     public BookEntity(String name, int year, String author, boolean rRated, String category, int quantity) {
