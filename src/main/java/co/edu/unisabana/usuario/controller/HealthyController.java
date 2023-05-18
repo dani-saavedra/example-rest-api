ac73c66de05ae9c68b5907089279ea71e91398f7
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2023-05-18 10:11:52 -0500

    Agrega servicio q tumba aplicacion

--- src/main/java/co/edu/unisabana/usuario/controller/HealthyController.java
+++ src/main/java/co/edu/unisabana/usuario/controller/HealthyController.java
@@ -1,13 +1,25 @@
 package co.edu.unisabana.usuario.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class HealthyController {
+
+    private ApplicationContext context;
 
     @GetMapping(path = "/ping")
     public String pong() {
         return "Pong";
+    }
+
+
+    @GetMapping(path = "/destroy")
+    public void destroy() {
+        ((ConfigurableApplicationContext) context).close();
     }
 }
--- src/main/java/co/edu/unisabana/usuario/repository/dao/repository/BookRepository.java
+++ src/main/java/co/edu/unisabana/usuario/repository/dao/repository/BookRepository.java
@@ -4,4 +4,5 @@ import co.edu.unisabana.usuario.repository.dao.entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<BookEntity, Integer> {
+
 }
